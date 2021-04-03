e714f8e04b9b32b220a5e6c5134e9a01e61210f6
Sarehon Taylor <sarehontaylor@icloud.com>
2021-04-03 09:18:58 -0400

    working on code to write html files with css

--- sitegeneration.js
+++ sitegeneration.js
@@ -1,6 +1,18 @@
 const fs = require ('fs')
 function writeSite(html){
 fs.writeFile(index.html), html, err => {
-    
-}
-}
+if (err) {
+throw err    
+}    
+console.log("saved")
+copyFiles()
+})
+
+const copyFile = () => {
+    fs.copyFile('./style.css') err => {
+        if (err) {
+            console.log(err);
+    }
+            console.log('css copied')
+});
+    module.exports = wrtieSite, copyFile
