52d4843ea09e33f78c5568179b415418caaadbbf
Sarehon Taylor <sarehontaylor@icloud.com>
2021-04-03 23:25:46 -0400

    wokring on inquirer and require code

---
+++ index.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+const inquier = require('inquirer');
+const {
+    inflateRawSync
+}  = require('zlib');
+
+const Engineer = require('./library/Engineer')
+Const Employee = require()
+Const Intern = require()
+const Manager = require()
+
+const generatePage = require()
+
+const writeHTML = require()
