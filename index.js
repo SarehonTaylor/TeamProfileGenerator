243dc796c46b6a77dec990e15eccf1ebe5a73ba3
Sarehon Taylor <sarehontaylor@icloud.com>
2021-04-03 23:46:59 -0400

    working on prompts

--- index.js
+++ index.js
@@ -11,5 +11,24 @@ Const Intern = require()
 const Manager = require()
 
 const generatePage = require()
-
 const writeHTML = require()
+
+const team = []
+
+
+function getInfo () {
+    inquier.prompt([{
+            type: 'input'
+            name: "name"
+            message: "Employees Name"
+            validate: nameInput => {
+                if (nameInput) {
+                    if (nameInput) {
+                        return true;
+                    }   else {
+                        console.log("Please enter team members name.")
+                    }
+                }
+            }
+    }]
+}
